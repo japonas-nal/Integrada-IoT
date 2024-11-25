ec66ad8d41d91fccbd3025e0bb09ce3dec98b4d9
japonas-nal <mtadashibr@gmail.com>
2024-11-25 04:17:51 -0300

    Iot

---
+++ script.js
@@ -0,0 +1,10 @@
+function brir(){
+    document.getElementById('menu').style.display='flex'
+    document.getElementById('brir').style.display='none'
+    document.getElementById('char').style.display='block'
+}
+function char(){
+    document.getElementById('menu').style.display='none'
+    document.getElementById('brir').style.display='block'
+    document.getElementById('char').style.display='none'
+}
